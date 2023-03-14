e126572a78087d2b844027bcdd40432c5778205d
Bao Gia Pham <baogpham@kms-technology.com>
2023-03-14 23:18:47 +0700

    Download file

--- src/components/ModalAnt.jsx
+++ src/components/ModalAnt.jsx
@@ -1,7 +1,16 @@
 import React, { useState } from "react";
 import { Badge, Button, Descriptions, Modal } from "antd";
-import './GoogleButton.css';
+import "./GoogleButton.css";
 import { InfoOutlined } from "@ant-design/icons";
+import JSZip from "jszip";
+import { saveAs } from "file-saver";
+import {
+  getDownloadURL,
+  getMetadata,
+  getStorage,
+  listAll,
+  ref,
+} from "firebase/storage";
 
 const ModalAnt = ({ title }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -10,6 +19,60 @@ const ModalAnt = ({ title }) => {
   };
   const handleOk = () => {
     setIsModalOpen(false);
+  };
+
+  const downloadFolderAsZip = async () => {
+    const jszip = new JSZip();
+    const storage = getStorage();
+    const folderRef = ref(storage, "baopgse140382@fpt.edu.vn/PE1");
+    const folderRef2 = ref(storage, "baopgse140382@fpt.edu.vn/PE1/Given");
+    const folderRef3 = ref(storage, "baopgse140382@fpt.edu.vn/PE1/Testcase");
+    const folder = await listAll(folderRef);
+    const folder2 = await listAll(folderRef2);
+    const folder3 = await listAll(folderRef3);
+    const promises = folder.items
+      .map(async (item) => {
+        const file = await getMetadata(item);
+        const fileRef = ref(storage, item.fullPath);
+        const fileBlob = await getDownloadURL(fileRef).then((url) => {
+          return fetch(`https://gentle-temple-68806.herokuapp.com/${url}`).then(
+            (response) => response.blob()
+          );
+        });
+        jszip.file(file.name, fileBlob);
+      })
+      .reduce((acc, curr) => acc.then(() => curr), Promise.resolve());
+    await promises;
+    const promises2 = folder2.items
+      .map(async (item) => {
+        const givenFolder = jszip.folder("Given");
+        const file = await getMetadata(item);
+        const fileRef = ref(storage, item.fullPath);
+        const fileBlob = await getDownloadURL(fileRef).then((url) => {
+          return fetch(`https://gentle-temple-68806.herokuapp.com/${url}`).then(
+            (response) => response.blob()
+          );
+        });
+        givenFolder.file(file.name, fileBlob);
+      })
+      .reduce((acc, curr) => acc.then(() => curr), Promise.resolve());
+    await promises2;
+    const promises3 = folder3.items
+      .map(async (item) => {
+        const testCFolder = jszip.folder("Testcase");
+        const file = await getMetadata(item);
+        const fileRef = ref(storage, item.fullPath);
+        const fileBlob = await getDownloadURL(fileRef).then((url) => {
+          return fetch(`https://gentle-temple-68806.herokuapp.com/${url}`).then(
+            (response) => response.blob()
+          );
+        });
+        testCFolder.file(file.name, fileBlob);
+      })
+      .reduce((acc, curr) => acc.then(() => curr), Promise.resolve());
+    await promises3;
+    const zipBlob = await jszip.generateAsync({ type: "blob" });
+    saveAs(zipBlob, "download.zip");
   };
 
   return (
@@ -38,13 +101,17 @@ const ModalAnt = ({ title }) => {
           </Descriptions.Item>
           <Descriptions.Item label="Type">By computer</Descriptions.Item>
           <Descriptions.Item label="File">
-          <a className="container" href="https://firebasestorage.googleapis.com/v0/b/capstone-cft.appspot.com/o/VNR-source-mini-2.docx?alt=media&token=c16c8d87-6029-4bf0-9679-452a98594673">
-            <div className="row align-items-center">
-              <div className="col-auto">
-                <img src="https://banner2.cleanpng.com/20180611/wc/kisspng-computer-icons-file-explorer-internet-explorer-5b1e670f8e8603.7503591815287191195838.jpg" class="img-thumbnail rounded img-size" alt="Responsive image" />
+            <a onClick={downloadFolderAsZip} className="container">
+              <div className="row align-items-center">
+                <div className="col-auto">
+                  <img
+                    src="https://banner2.cleanpng.com/20180611/wc/kisspng-computer-icons-file-explorer-internet-explorer-5b1e670f8e8603.7503591815287191195838.jpg"
+                    className="img-thumbnail rounded img-size"
+                    alt="Responsive image"
+                  />
+                </div>
               </div>
-            </div>
-          </a>
+            </a>
           </Descriptions.Item>
         </Descriptions>
       </Modal>
