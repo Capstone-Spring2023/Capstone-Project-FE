90864f3a7b97fdeb221135fb1130fd52b5b3f2f9
Bao Gia Pham <baogpham@kms-technology.com>
2023-03-14 12:08:59 +0700

    Download file idea

--- src/components/ModalAnt.jsx
+++ src/components/ModalAnt.jsx
@@ -1,10 +1,16 @@
 import React, { useState } from "react";
 import { Badge, Button, Descriptions, Modal } from "antd";
-import './GoogleButton.css';
+import "./GoogleButton.css";
 import { InfoOutlined } from "@ant-design/icons";
-import {getStorage,listAll,uploadBytes, getDownloadURL, ref, uploadBytesResumable, getMetadata, getBlob } from "firebase/storage";
+import {
+  getDownloadURL,
+  getMetadata,
+  getStorage,
+  listAll,
+  ref,
+} from "firebase/storage";
 import JSZip from "jszip";
-import {saveAs, SaveAs} from "file-saver";
+import { saveAs } from "file-saver";
 
 const ModalAnt = ({ title }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -18,27 +24,32 @@ const ModalAnt = ({ title }) => {
   const downloadFolderAsZip = async () => {
     const jszip = new JSZip();
     const storage = getStorage();
-    
-    const folderRef = ref(
-      storage,
-      'gs://capstone-cft.appspot.com/phatntse140192@fpt.edu.vn/PE1'
-    );
+
+    const folderRef = ref(storage, "baopgse140382@fpt.edu.vn/PE1");
     const folder = await listAll(folderRef);
     const promises = folder.items
       .map(async (item) => {
         const file = await getMetadata(item);
         const fileRef = ref(storage, item.fullPath);
+        console.log("FILE", file);
+        console.log("FILEREF", fileRef);
         const fileBlob = await getDownloadURL(fileRef).then((url) => {
-          return fetch(`https://cors-anywhere.herokuapp.com/${url}`).then((response) => response.blob());
+          return fetch(`https://cors-anywhere.herokuapp.com/${url}`).then(
+            (response) => response.blob()
+          );
         });
+        console.log("FILEBLOB", fileBlob);
+        console.log("FOLDER", folder.prefixes[0]._location.path);
+        jszip.folder("Given/");
+        jszip.folder("Test_Case/");
         jszip.file(file.name, fileBlob);
       })
       .reduce((acc, curr) => acc.then(() => curr), Promise.resolve());
     await promises;
-  
-    const zipBlob = await jszip.generateAsync({ type: 'blob' });
-    saveAs(zipBlob, 'download.zip');
-  }
+
+    const zipBlob = await jszip.generateAsync({ type: "blob" });
+    saveAs(zipBlob, "download.zip");
+  };
 
   return (
     <>
@@ -66,14 +77,18 @@ const ModalAnt = ({ title }) => {
           </Descriptions.Item>
           <Descriptions.Item label="Type">By computer</Descriptions.Item>
           <Descriptions.Item label="File">
-          <a className="container" onClick={downloadFolderAsZip}>
-          {/* https://firebasestorage.googleapis.com/v0/b/capstone-cft.appspot.com/o/VNR-source-mini-2.docx?alt=media&token=c16c8d87-6029-4bf0-9679-452a98594673 */}
-            <div className="row align-items-center">
-              <div className="col-auto">
-                <img src="https://banner2.cleanpng.com/20180611/wc/kisspng-computer-icons-file-explorer-internet-explorer-5b1e670f8e8603.7503591815287191195838.jpg" class="img-thumbnail rounded img-size" alt="Responsive image" />
+            <a className="container" onClick={downloadFolderAsZip}>
+              {/* https://firebasestorage.googleapis.com/v0/b/capstone-cft.appspot.com/o/VNR-source-mini-2.docx?alt=media&token=c16c8d87-6029-4bf0-9679-452a98594673 */}
+              <div className="row align-items-center">
+                <div className="col-auto">
+                  <img
+                    src="https://banner2.cleanpng.com/20180611/wc/kisspng-computer-icons-file-explorer-internet-explorer-5b1e670f8e8603.7503591815287191195838.jpg"
+                    className="img-thumbnail rounded img-size"
+                    alt="Responsive image"
+                  />
+                </div>
               </div>
-            </div>
-          </a>
+            </a>
           </Descriptions.Item>
         </Descriptions>
       </Modal>
