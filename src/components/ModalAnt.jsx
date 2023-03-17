b161af60b2f2370a67510d00755bc815b06aba70
Bao Tram Zn <baogpham@kms-technology.com>
2023-03-17 22:51:37 +0700

    Update UI

--- src/components/ModalAnt.jsx
+++ src/components/ModalAnt.jsx
@@ -1,135 +1,161 @@
-import React, { useState } from "react";
-import { Badge, Button, Descriptions, Modal } from "antd";
+import React, {useState} from "react";
+import {Badge, Button, Descriptions, Modal, Tooltip} from "antd";
 import "./GoogleButton.css";
-import { InfoOutlined } from "@ant-design/icons";
+import {InfoOutlined} from "@ant-design/icons";
 import {
-  getDownloadURL,
-  getMetadata,
-  getStorage,
-  listAll,
-  ref,
+    getDownloadURL,
+    getMetadata,
+    getStorage,
+    listAll,
+    ref,
 } from "firebase/storage";
 import JSZip from "jszip";
-import { saveAs } from "file-saver";
+import {saveAs} from "file-saver";
 
-const ModalAnt = ({ title }) => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const showModal = () => {
-    setIsModalOpen(true);
-  };
-  const handleOk = () => {
-    setIsModalOpen(false);
-  };
-  const [isZipping, setIsZipping] = useState(false);
-  const [isDownloading, setIsDownloading] = useState(false);
+const ModalAnt = ({title, id}) => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [examTitle, setExamTitle] = useState("");
+    const [lecturerName, setLecturerName] = useState("");
+    const [subjectName, setSubjectName] = useState("");
+    const showModal = () => {
+        getDetail();
+        setIsModalOpen(true);
+    };
+    const handleOk = () => {
+        setIsModalOpen(false);
+    };
+    const [isZipping, setIsZipping] = useState(false);
+    const [isDownloading, setIsDownloading] = useState(false);
 
-  const downloadFolderAsZip = async () => {
-    setIsZipping(true);
-    const jszip = new JSZip();
-    const storage = getStorage();
-    const folderRef = ref(storage, "baopgse140382@fpt.edu.vn/PE1");
-    const folderRef2 = ref(storage, "baopgse140382@fpt.edu.vn/PE1/Given");
-    const folderRef3 = ref(storage, "baopgse140382@fpt.edu.vn/PE1/Testcase");
-    const folder = await listAll(folderRef);
-    const folder2 = await listAll(folderRef2);
-    const folder3 = await listAll(folderRef3);
-    const promises = folder.items
-      .map(async (item) => {
-        const file = await getMetadata(item);
-        const fileRef = ref(storage, item.fullPath);
-        console.log("FILE", file);
-        console.log("FILEREF", fileRef);
-        const fileBlob = await getDownloadURL(fileRef).then((url) => {
-          return fetch(`https://gentle-temple-68806.herokuapp.com/${url}`).then(
-            (response) => response.blob()
-          );
-        });
-        console.log("FILEBLOB", fileBlob);
-        console.log("FOLDER", folder.prefixes[0]._location.path);
-        jszip.folder("Given/");
-        jszip.folder("Test_Case/");
-        jszip.file(file.name, fileBlob);
-      })
-      .reduce((acc, curr) => acc.then(() => curr), Promise.resolve());
-    await promises;
-    const promises2 = folder2.items
-      .map(async (item) => {
-        const givenFolder = jszip.folder("Given");
-        const file = await getMetadata(item);
-        const fileRef = ref(storage, item.fullPath);
-        const fileBlob = await getDownloadURL(fileRef).then((url) => {
-          return fetch(`https://gentle-temple-68806.herokuapp.com/${url}`).then(
-            (response) => response.blob()
-          );
-        });
-        givenFolder.file(file.name, fileBlob);
-      })
-      .reduce((acc, curr) => acc.then(() => curr), Promise.resolve());
-    await promises2;
-    const promises3 = folder3.items
-      .map(async (item) => {
-        const testCFolder = jszip.folder("Testcase");
-        const file = await getMetadata(item);
-        const fileRef = ref(storage, item.fullPath);
-        const fileBlob = await getDownloadURL(fileRef).then((url) => {
-          return fetch(`https://gentle-temple-68806.herokuapp.com/${url}`).then(
-            (response) => response.blob()
-          );
-        });
-        testCFolder.file(file.name, fileBlob);
-      })
-      .reduce((acc, curr) => acc.then(() => curr), Promise.resolve());
-    await promises3;
-    setIsZipping(false);
-    setIsDownloading(true);
-    const zipBlob = await jszip.generateAsync({ type: "blob" });
-    saveAs(zipBlob, "PE1.zip");
-    setIsDownloading(false);
-  };
+    const getDetail = () => {
+        fetch("https://fpt-cft.azurewebsites.net/v1/api/exams/" + id, {
+            method: "GET",
+            headers: {"Content-Type": "application/json"},
+        })
+            .then((res) => {
+                return res.json();
+            })
+            .then((resp) => {
+                console.log("RES", resp);
+                setExamTitle(resp.examContent);
+                setLecturerName(resp.lecturerName);
+                setSubjectName(resp.subjectName)
+            })
+            .catch((err) => {
+                console.log(err.message);
+            });
+    };
 
-  return (
-    <>
-      <InfoOutlined
-        onClick={showModal}
-        style={{ fontSize: 17, color: "blue" }}
-        height={55}
-      />
-      <Modal
-        centered={true}
-        open={isModalOpen}
-        title={`${title}`}
-        onOk={handleOk}
-        footer={[
-          <Button key="submit" type="default" onClick={handleOk}>
-            OK
-          </Button>,
-        ]}
-      >
-        <Descriptions layout="vertical">
-          <Descriptions.Item label="UserName">HoaDNT</Descriptions.Item>
-          <Descriptions.Item label="Title">PRF exam</Descriptions.Item>
-          <Descriptions.Item label="Status">
-            <Badge status="success" text="Active" />
-          </Descriptions.Item>
-          <Descriptions.Item label="Type">By computer</Descriptions.Item>
-          <Descriptions.Item label="File">
-            <a className="container" onClick={downloadFolderAsZip}>
-              <div className="row align-items-center">
-                <div className="col-auto">
-                  <img
-                    src="https://banner2.cleanpng.com/20180611/wc/kisspng-computer-icons-file-explorer-internet-explorer-5b1e670f8e8603.7503591815287191195838.jpg"
-                    className="img-thumbnail rounded img-size"
-                    alt="Responsive image"
-                  />
-                </div>
-              </div>
-              {isZipping && <div>Đang tải file...</div>}
-            </a>
-          </Descriptions.Item>
-        </Descriptions>
-      </Modal>
-    </>
-  );
+    const downloadFolderAsZip = async () => {
+        setIsZipping(true);
+        const jszip = new JSZip();
+        const storage = getStorage();
+        const folderRef = ref(storage, "baopgse140382@fpt.edu.vn/PE1");
+        const folderRef2 = ref(storage, "baopgse140382@fpt.edu.vn/PE1/Given");
+        const folderRef3 = ref(storage, "baopgse140382@fpt.edu.vn/PE1/Testcase");
+        const folder = await listAll(folderRef);
+        const folder2 = await listAll(folderRef2);
+        const folder3 = await listAll(folderRef3);
+        const promises = folder.items
+            .map(async (item) => {
+                const file = await getMetadata(item);
+                const fileRef = ref(storage, item.fullPath);
+                console.log("FILE", file);
+                console.log("FILEREF", fileRef);
+                const fileBlob = await getDownloadURL(fileRef).then((url) => {
+                    return fetch(`https://gentle-temple-68806.herokuapp.com/${url}`).then(
+                        (response) => response.blob()
+                    );
+                });
+                console.log("FILEBLOB", fileBlob);
+                console.log("FOLDER", folder.prefixes[0]._location.path);
+                jszip.folder("Given/");
+                jszip.folder("Test_Case/");
+                jszip.file(file.name, fileBlob);
+            })
+            .reduce((acc, curr) => acc.then(() => curr), Promise.resolve());
+        await promises;
+        const promises2 = folder2.items
+            .map(async (item) => {
+                const givenFolder = jszip.folder("Given");
+                const file = await getMetadata(item);
+                const fileRef = ref(storage, item.fullPath);
+                const fileBlob = await getDownloadURL(fileRef).then((url) => {
+                    return fetch(`https://gentle-temple-68806.herokuapp.com/${url}`).then(
+                        (response) => response.blob()
+                    );
+                });
+                givenFolder.file(file.name, fileBlob);
+            })
+            .reduce((acc, curr) => acc.then(() => curr), Promise.resolve());
+        await promises2;
+        const promises3 = folder3.items
+            .map(async (item) => {
+                const testCFolder = jszip.folder("Testcase");
+                const file = await getMetadata(item);
+                const fileRef = ref(storage, item.fullPath);
+                const fileBlob = await getDownloadURL(fileRef).then((url) => {
+                    return fetch(`https://gentle-temple-68806.herokuapp.com/${url}`).then(
+                        (response) => response.blob()
+                    );
+                });
+                testCFolder.file(file.name, fileBlob);
+            })
+            .reduce((acc, curr) => acc.then(() => curr), Promise.resolve());
+        await promises3;
+        setIsZipping(false);
+        setIsDownloading(true);
+        const zipBlob = await jszip.generateAsync({type: "blob"});
+        saveAs(zipBlob, "PE1.zip");
+        setIsDownloading(false);
+    };
+
+    return (
+        <>
+            <Tooltip title="Info">
+                <InfoOutlined
+                    onClick={showModal}
+                    style={{fontSize: 17, color: "blue"}}
+                    height={55}
+                />
+                <Modal
+                    centered={true}
+                    open={isModalOpen}
+                    title={`${title}`}
+                    onOk={handleOk}
+                    onCancel={handleOk}
+                    footer={[
+                        <Button key="submit" type="default" onClick={handleOk}>
+                            OK
+                        </Button>,
+                    ]}
+                >
+                    <Descriptions layout="vertical">
+                        <Descriptions.Item label="UserName">{lecturerName}</Descriptions.Item>
+                        <Descriptions.Item label="Title">{examTitle}</Descriptions.Item>
+                        <Descriptions.Item label="Status">
+                            <Badge status="success" text="Active"/>
+                        </Descriptions.Item>
+                        <Descriptions.Item label="Type">By computer</Descriptions.Item>
+                        <Descriptions.Item label="File">
+                            <a className="container" onClick={downloadFolderAsZip}>
+                                <div className="row align-items-center">
+                                    <div className="col-auto">
+                                        <img
+                                            src="https://banner2.cleanpng.com/20180611/wc/kisspng-computer-icons-file-explorer-internet-explorer-5b1e670f8e8603.7503591815287191195838.jpg"
+                                            className="img-thumbnail rounded img-size"
+                                            alt="Responsive image"
+                                        />
+                                    </div>
+                                </div>
+                                {isZipping && <div>Đang tải file...</div>}
+                            </a>
+                        </Descriptions.Item>
+                    </Descriptions>
+                </Modal>
+            </Tooltip>
+        </>
+    );
 };
 
 export default ModalAnt;
--- src/pages/ExamSubmissionView.jsx
+++ src/pages/ExamSubmissionView.jsx
@@ -3,11 +3,10 @@ import useTable from "../hooks/useTable";
 import { toast, Toaster } from "react-hot-toast";
 import { Header, ModalAnt, Popup } from "../components";
 import avatar from "../assets/banner.jpg";
-import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 import TableFooter from "../components/Table/TableFooter";
-import { ACTIVE_GREEN, RED } from "../utils/constants";
-import { StyleProvider } from "@ant-design/cssinjs";
-import { ConfigProvider, Tooltip } from "antd";
+import { ACTIVE_GREEN } from "../utils/constants";
+import { Popconfirm, Tooltip } from "antd";
+import { CheckOutlined } from "@ant-design/icons";
 
 const ExamSubmissionView = () => {
   const [page, setPage] = useState(1);
@@ -20,36 +19,34 @@ const ExamSubmissionView = () => {
   }, []);
 
   const handleApprove = (id) => {
-    if (window.confirm("Do you want to approve this exam?")) {
-      const data = {
-        commentModel: {
-          leaderId: 2,
-          examPaperId: id,
-          commentContent: "string"
-        },
-        examUpdateApproveModel: {
-          isApproved: true
-        }
-      };
-      toast.promise(
-        fetch("https://fpt-cft.azurewebsites.net/v1/api/exams/review-exam", {
-          method: "PUT",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(data)
+    const data = {
+      commentModel: {
+        leaderId: 2,
+        examPaperId: id,
+        commentContent: "string",
+      },
+      examUpdateApproveModel: {
+        isApproved: true,
+      },
+    };
+    toast.promise(
+      fetch("https://fpt-cft.azurewebsites.net/v1/api/exams/review-exam", {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+      })
+        .then((resp) => {
+          fetchTable();
         })
-          .then((resp) => {
-            fetchTable();
-          })
-          .catch((err) => {
-            console.log(err.message);
-          }),
-        {
-          loading: "Approving...",
-          success: <b>Approve successfully</b>,
-          error: <b>Approve fail</b>,
-        }
-      );
-    }
+        .catch((err) => {
+          console.log(err.message);
+        }),
+      {
+        loading: "Approving...",
+        success: <b>Approve successfully</b>,
+        error: <b>Approve fail</b>,
+      }
+    );
   };
 
   const fetchTable = () => {
@@ -111,7 +108,9 @@ const ExamSubmissionView = () => {
                     <div className="font-medium text-gray-700">
                       Lecturer: {item.examPaperId}
                     </div>
-                    <div className="text-gray-400">Subject: {item.registerSubjectId}</div>
+                    <div className="text-gray-400">
+                      Subject: {item.registerSubjectId}
+                    </div>
                   </div>
                 </td>
                 <td className="px-3 py-3">{item.examContent}</td>
@@ -134,54 +133,31 @@ const ExamSubmissionView = () => {
                 </td>
                 <td className="px-6 py-4">
                   <div className="flex justify-start gap-4 content-center items-center">
-                    <TooltipComponent content="Approve" position="BottomCenter">
-                      <a onClick={() => handleApprove(item.examPaperId)}>
-                        <svg
-                          className="svg-icon"
-                          viewBox="0 0 20 20"
-                          width={15}
-                          height={15}
-                        >
-                          <path
-                            fill={`${ACTIVE_GREEN}`}
-                            d="M7.197,16.963H7.195c-0.204,0-0.399-0.083-0.544-0.227l-6.039-6.082c-0.3-0.302-0.297-0.788,0.003-1.087
-							C0.919,9.266,1.404,9.269,1.702,9.57l5.495,5.536L18.221,4.083c0.301-0.301,0.787-0.301,1.087,0c0.301,0.3,0.301,0.787,0,1.087
-							L7.741,16.738C7.596,16.882,7.401,16.963,7.197,16.963z"
-                          ></path>
-                        </svg>
-                      </a>
-                    </TooltipComponent>
+                    <Tooltip title="Approve">
+                      <Popconfirm
+                        title="Approve the exam"
+                        description="Are you sure to approve this exam?"
+                        onConfirm={() => handleApprove(item.examPaperId)}
+                        okText="Yes"
+                        cancelText="No"
+                      >
+                        <CheckOutlined
+                          style={{ fontSize: 17, color: "lightgreen" }}
+                          height={55}
+                        />
+                      </Popconfirm>
+                    </Tooltip>
                     <Tooltip title="Reject">
-                      <ConfigProvider
-                        theme={{
-                          token: {
-                            colorPrimary: "#ea1c1c",
-                          },
-                        }}
-                      >
-                        <StyleProvider hashPriority="high">
-                          <Popup
-                            title="Comment"
-                            fetchTable={fetchTable}
-                            id={item.examPaperId}
-                          />
-                        </StyleProvider>
-                      </ConfigProvider>
+                      <Popup
+                        title="Comment"
+                        fetchTable={fetchTable}
+                        id={item.examPaperId}
+                      />
                     </Tooltip>
-
-                    <Tooltip title="Info">
-                      <ConfigProvider
-                        theme={{
-                          token: {
-                            colorPrimary: "#ea1c1c",
-                          },
-                        }}
-                      >
-                        <StyleProvider hashPriority="high">
-                          <ModalAnt title="Exam submission detail" />
-                        </StyleProvider>
-                      </ConfigProvider>
-                    </Tooltip>
+                    <ModalAnt
+                      title="Exam submission detail"
+                      id={item.examPaperId}
+                    />
                   </div>
                 </td>
               </tr>
