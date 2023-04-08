7b87e9ec63b8b7d9020b1bdfc91c7cce70b77faa
Bao Tram Zn <baogpham@kms-technology.com>
2023-04-08 10:57:08 +0700

    Update Schedule

--- src/pages/Schedule.jsx
+++ src/pages/Schedule.jsx
@@ -144,15 +144,9 @@ const Schedule = () => {
   const startOfWeekDate = startOfWeek(startDate);
   const endOfWeekDate = endOfWeek(startDate);
 
-    const handleStartDateChange = (e) => {
-        setStartDate(new Date(e.target.value));
-    };
-
-    const daysOfMonth = [];
-    for (let i = 0; i < 7; i++) {
-        const day = addDays(startOfWeekDate, i);
-        daysOfMonth.push(moment(day).format("DD-MM-YYYY"));
-    }
+  const handleStartDateChange = (e) => {
+    setStartDate(new Date(e.target.value));
+  };
 
   const daysOfMonth = [];
   for (let i = 0; i < 7; i++) {
@@ -174,145 +168,154 @@ const Schedule = () => {
 
   const [schedule, setSchedule] = useState([]);
 
-
   useEffect(() => {
     fetchSchedule();
   }, []);
-  
-    const fetchSchedule = () => {
-        fetch(`${BASE_URL_API}/schedule/lecturer/2/schedule`)
-            .then((res) => {
-                return res.json();
-            })
-            .then((resp) => {
-                const timetable = new Array(8).fill(null).map(() => new Array(7).fill(null));
-                const daysOfMonth = [];
-                const startOfWeekDate = startOfWeek(new Date(), { weekStartsOn: 1 });
-                for (let i = 0; i < 7; i++) {
-                    const day = addDays(startOfWeekDate, i);
-                    daysOfMonth.push(moment(day).format("DD-MM-YYYY"));
-                }
-                for (const scheduleItem of resp) {
-                    const scheduleDate = moment(scheduleItem.scheduleDate, "YYYY-MM-DDTHH:mm:ss").format("DD-MM-YYYY");
-                    const dayIndex = daysOfMonth.indexOf(scheduleDate);
-                    if (dayIndex >= 0) {
-                        const slotIndex = scheduleItem.slot - 1;
-                        timetable[slotIndex][dayIndex] = {
-                            scheduleId: scheduleItem.scheduleId,
-                            classCode: scheduleItem.classCode,
-                            classId: scheduleItem.classId,
-                            slot: scheduleItem.slot,
-                            scheduleDate: scheduleDate,
-                        };
-                    }
-                }
-                setSchedule(timetable);
-            })
-            .catch((err) => {
-                console.log(err.message);
-            });
-    };
 
-    for (let i = 0; i < timetable.length; i++) {
-        for (let j = 0; j < timetable[i].length; j++) {
-            const lesson = timetable[i][j];
-            if (lesson !== null) {
-                timetable[i][j] = (
-                    <td key={`${daysOfMonth[j]}-${lesson.slot}`}>
-                        <div className="schedule-item">
-                            <div className="schedule-time">{moment(lesson.scheduleDate).format("HH:mm")}</div>
-                            {daysOfMonth[j] === moment(lesson.scheduleDate).format("DD-MM-YYYY") && (
-                                <div className="schedule-class">{lesson.classCode}</div>
-                            )}
-                        </div>
-                    </td>
-                );
-            } else {
-                timetable[i][j] = <td key={`${daysOfMonth[j]}-null`} />;
-            }
-        }
+  const fetchSchedule = () => {
+    fetch(`${BASE_URL_API}/schedule/lecturer/2/schedule`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((resp) => {
+        console.log("RESP", resp);
+        const formattedSchedule = resp.map((scheduleItem) => {
+          const scheduleDate = moment(
+            scheduleItem.scheduleDate,
+            "YYYY-MM-DDTHH:mm:ss"
+          ).format("YYYY-MM-DD HH:mm:ss");
+          console.log("DATE", scheduleDate);
+          const slotIndex = scheduleItem.slot - 1;
+          const dayIndex = daysOfMonth.indexOf(scheduleDate);
+          timetable[slotIndex] = timetable[slotIndex] || [];
+          timetable[slotIndex][dayIndex] = {
+            scheduleId: scheduleItem.scheduleId,
+            classCode: scheduleItem.classCode,
+            classId: scheduleItem.classId,
+            slot: scheduleItem.slot,
+            scheduleDate: scheduleDate,
+          };
+        });
+        setSchedule(formattedSchedule);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+
+  for (const element of timetable) {
+    for (let j = 0; j < element.length; j++) {
+      const lesson = element[j];
+      // console.log(lesson);
+      if (lesson !== null) {
+        element[j] = (
+          <td key={`${daysOfMonth[j]}-${lesson.slot}`}>
+            <div className="schedule-item">
+              <div className="schedule-time">{`Slot ${lesson.slot} - ${moment(
+                lesson.scheduleDate
+              ).format("HH:mm")}`}</div>
+              <div className="schedule-class">{lesson.classCode}</div>
+            </div>
+          </td>
+        );
+      } else {
+        element[j] = <td key={`${daysOfMonth[j]}-null`} />;
+      }
+
+      // console.log(`timetable[${i}][${j}] = ${timetable[i][j]}`);
     }
   }
 
-    return (
-        <>
-            <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
-                <div className="app-container">
-                    <div className="container">
-                        <div className="header">
-                            <h2>Timetable</h2>
-                        </div>
-                        <table className="table">
-                            <thead>
-                                <tr>
-                                    <th>
-                                        <input
-                                            type="date"
-                                            value={format(startDate, "yyyy-MM-dd")}
-                                            onChange={handleStartDateChange}
-                                            className="date-picker"
-                                        />
-                                    </th>
-                                    {daysOfMonth.map((day, index) => (
-                                        <th key={`${day}-${daysOfWeek[index]}`} className="text-center">
-                                            <div className="day-container">
-                                                <div className="day">{daysOfWeek[index]}</div>
-                                                <div className="date">{day}</div>
-                                            </div>
-                                        </th>
-                                    ))}
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {slots.map((slot, index) => (
-                                    <tr key={`slot-row-${index}`}>
-                                        {slot}
-                                        {daysOfMonth.map((day, dayIndex) => {
-                                            const lesson = timetable[index][dayIndex];
-                                            return (
-                                                <td key={`slot-${index}-day-${dayIndex}`}
-                                                className={`td-style ${lesson ? "bg-gray-100" :"bg-white"}`}>
-                                                    {lesson && (
-                                                        <div className="lesson">
-                                                            {/* <div className='lesson-code'>
-                                                                {lesson.classCode||lesson.classId}
-                                                            </div> */}
-                                                        </div>
-                                                    )}
-                                                </td>
-                                            );
-                                        })}
-
-                                    </tr>
-                                ))}
-                            </tbody>
-
-                        </table>
-                    </div>
-                </div>
+  return (
+    <>
+      <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+        <div className="app-container">
+          <div className="container">
+            <div className="header">
+              <h2>Timetable</h2>
             </div>
-            <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
-                <>
-                    Import file schedule
-                    <Form.Item name="file" accept=".docx">
-                        <Dragger customRequest={(e) => upLoadFile(e)}>
-                            <p className="ant-upload-drag-icon">
-                                <InboxOutlined />
-                            </p>
-                            <p className="ant-upload-text">Click or drag file to this area to upload</p>
-                            <p className="ant-upload-hint">Only support for excel file</p>
-                        </Dragger>
-                    </Form.Item>
-                    <Button key="submit" type="default" onClick={handleOk2}>
-                        Submit
-                    </Button>
-                </>
-                Import file excel
-                <div>
-                    <input type="file" onChange={handleFile} />
-                </div>
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>
+                    <input
+                      type="date"
+                      value={format(startDate, "yyyy-MM-dd")}
+                      onChange={handleStartDateChange}
+                      className="date-picker"
+                    />
+                  </th>
+                  {daysOfMonth.map((day, index) => (
+                    <th
+                      key={`${day}-${daysOfWeek[index]}`}
+                      className="text-center"
+                    >
+                      <div className="day-container">
+                        <div className="day">{daysOfWeek[index]}</div>
+                        <div className="date">{day}</div>
+                      </div>
+                    </th>
+                  ))}
+                </tr>
+              </thead>
+              <tbody>
+                {slots.map((slot, index) => (
+                  <tr key={`slot-row-${index}`}>
+                    {slot}
+                    {daysOfMonth.map((day, dayIndex) => {
+                      console.log("DAY", day);
+                      console.log("DAYINDEX", dayIndex);
+                      console.log("SLOT", slot);
+                      const lesson = timetable[index][dayIndex];
+                      console.log("lesson", lesson);
+                      return (
+                        <td
+                          key={`slot-${index}-day-${dayIndex}`}
+                          className={`td-style ${
+                            lesson ? "bg-gray-100" : "bg-white"
+                          }`}
+                        >
+                          {lesson &&
+                            "2023-04-12" === day &&
+                            "slot-6" === slot.key && (
+                              <div className="lesson">
+                                <div className="lesson-code">{lesson.key}</div>
+                              </div>
+                            )}
+                        </td>
+                      );
+                    })}
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+      <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+        <>
+          Import file schedule
+          <Form.Item name="file" accept=".docx">
+            <Dragger customRequest={(e) => upLoadFile(e)}>
+              <p className="ant-upload-drag-icon">
+                <InboxOutlined />
+              </p>
+              <p className="ant-upload-text">
+                Click or drag file to this area to upload
+              </p>
+              <p className="ant-upload-hint">Only support for excel file</p>
+            </Dragger>
+          </Form.Item>
+          <Button key="submit" type="default" onClick={handleOk2}>
+            Submit
+          </Button>
+        </>
+        Import file excel
+        <div>
+          <input type="file" onChange={handleFile} />
+        </div>
       </div>
     </>
-    );
+  );
 };
 export default Schedule;
