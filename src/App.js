f6da482565c181417656761e9f16a5bbb3e3acc4
Bao Tram Zn <baogpham@kms-technology.com>
2023-04-08 10:57:40 +0700

    Update App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { lazy, Suspense, useEffect, useState } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React, { lazy, Suspense, useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
 import { FiSettings } from "react-icons/fi";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 import { useStateContext } from "./contexts/ContextProvider";
@@ -32,10 +32,10 @@ import SubjectCreate from "./section/Subjects/SubjectCreate";
 import SubjectEdit from "./section/Subjects/SubjectEdit";
 import Spinner from "./components/Spinner";
 import io from "socket.io-client";
-import NotiPopup from "./components/NotiPopup";
 import { SOCKET_URL } from "./utils/constants";
 
 const socket = io(`${SOCKET_URL}`);
+// const socket = io("http://localhost:4000");
 const App = () => {
   const {
     activeMenu,
@@ -66,91 +66,90 @@ const App = () => {
 
   return (
     <div className={currentMode === "Dark" ? "dark" : ""}>
-      <BrowserRouter>
-        <div className="flex relative dark:bg-main-dark-bg">
-          <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
-            <TooltipComponent content="Settings" position="Top">
-              <button
-                type="button"
-                className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white"
-                style={{ background: currentColor, borderRadius: "50%" }}
-                onClick={() => setThemeSettings(true)}
-              >
-                <FiSettings />
-              </button>
-            </TooltipComponent>
+      <div className="flex relative dark:bg-main-dark-bg">
+        <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
+          <TooltipComponent content="Settings" position="Top">
+            <button
+              type="button"
+              className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white"
+              style={{ background: currentColor, borderRadius: "50%" }}
+              onClick={() => setThemeSettings(true)}
+            >
+              <FiSettings />
+            </button>
+          </TooltipComponent>
+        </div>
+        {activeMenu && !isLoginPage ? (
+          <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
+            <Sidebar />
           </div>
+        ) : (
+          <></>
+        )}
+        <div
+          className={
+            activeMenu && !isLoginPage
+              ? "dark:bg-main-dark-bg bg-main-bg min-h-screen md:ml-72 w-full"
+              : "bg-main-bg dark:bg-main-dark-bg w-full min-h-screen flex-2"
+          }
+        >
           {activeMenu && !isLoginPage ? (
-            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
-              <Sidebar />
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
+              <Navbar />
             </div>
           ) : (
             <></>
           )}
-          <div
-            className={
-              activeMenu && !isLoginPage
-                ? "dark:bg-main-dark-bg bg-main-bg min-h-screen md:ml-72 w-full"
-                : "bg-main-bg dark:bg-main-dark-bg w-full min-h-screen flex-2"
-            }
-          >
-            {activeMenu && !isLoginPage ? (
-              <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
-                <Navbar />
-              </div>
-            ) : (
-              <></>
-            )}
 
-            <div>
-              {themeSettings && <ThemeSettings />}
-              <Suspense fallback={<Spinner />}>
-                <Routes>
-                  {/*Login*/}
-                  <Route path="/" element={<LoginPage />} />
+          <div>
+            {themeSettings && <ThemeSettings />}
+            <Suspense fallback={<Spinner />}>
+              <Routes>
+                {/*Login*/}
+                <Route path="/" element={<LoginPage />} />
 
-                  {/*Dashboard*/}
-                  <Route
-                    path="/overview"
-                    element={
-                      <>
-                        {/*<NotiFirebase />*/}
-                        <DashboardLazy />
-                        {/*{isShowNoti && (*/}
-                        {/*  <NotiPopup*/}
-                        {/*    title="You have exam schedule request from"*/}
-                        {/*    body="Hoa DNT"*/}
-                        {/*  />*/}
-                        {/*)}*/}
-                      </>
-                    }
-                  />
+                {/*Dashboard*/}
+                <Route
+                  path="/overview"
+                  element={
+                    <>
+                      {/*<NotiFirebase />*/}
+                      <DashboardLazy />
+                      {/*{isShowNoti && (*/}
+                      {/*  <NotiPopup*/}
+                      {/*    title="You have exam schedule request from"*/}
+                      {/*    body="Hoa DNT"*/}
+                      {/*  />*/}
+                      {/*)}*/}
+                    </>
+                  }
+                />
 
-                  {/*Pages*/}
-                  {sessionStorage.getItem("roleName") === "Lecturer" ? (
+                {/*Pages*/}
+                {sessionStorage.getItem("roleName") === "Lecturer" ? (
                   <Route
                     path="/exam-submission"
                     element={<ExamsSubmission />}
                   />
-                  ) : null}
-                  {sessionStorage.getItem("roleName") === "Leader" ? (
+                ) : null}
+                {sessionStorage.getItem("roleName") === "Leader" ? (
                   <Route path="/exam-schedule" element={<ExamsSchedule />} />
-                  ) : null}
-                  {sessionStorage.getItem("roleName") === "Lecturer" ? (
+                ) : null}
+                {sessionStorage.getItem("roleName") === "Lecturer" ? (
                   <Route
                     path="/exam-schedule-view"
                     element={<ExamsScheduleView />}
                   />
-                  ) : null}
-                  {sessionStorage.getItem("roleName") === "Leader" ? (
+                ) : null}
+                {sessionStorage.getItem("roleName") === "Leader" ? (
                   <Route
                     path="/exam-submission-view"
                     element={<ExamSubmissionView />}
                   />
-                  ) : null}
-                  <Route path="/leaders" element={<Leaders />} />
-                  <Route path="/schedules" element={<Schedule />} />
-                  <Route path="/subjects" element={<Subjects />} />
+                ) : null}
+                <Route path="/leaders" element={<Leaders />} />
+                <Route path="/schedules" element={<Schedule />} />
+                <Route path="/subjects" element={<Subjects />} />
 
                   {/*Apps*/}
                   <Route path="/calendar" element={<Calendar />} />
@@ -162,48 +161,44 @@ const App = () => {
                   />
                   <Route path="/available-subject" element={<LeaderSubject />} />
 
-                  {/*Exams Submission*/}
-                  <Route
-                    path="/exam/submission/create"
-                    element={<ExamSubmissionCreate />}
-                  />
-                  <Route
-                    path="/exam/submission/edit/:examPaperId"
-                    element={<ExamSubmissionEdit />}
-                  />
+                {/*Exams Submission*/}
+                <Route
+                  path="/exam/submission/create"
+                  element={<ExamSubmissionCreate />}
+                />
+                <Route
+                  path="/exam/submission/edit/:examPaperId"
+                  element={<ExamSubmissionEdit />}
+                />
 
-                  {/*Exams Schedule*/}
-                  <Route
-                    path="/exam/schedule/create"
-                    element={<ExamScheduleCreate socket={socket} />}
-                  />
-                  <Route
-                    path="/exam/schedule/edit/:availableSubjectId"
-                    element={<ExamScheduleEdit />}
-                  />
+                {/*Exams Schedule*/}
+                <Route
+                  path="/exam/schedule/create"
+                  element={<ExamScheduleCreate socket={socket} />}
+                />
+                <Route
+                  path="/exam/schedule/edit/:availableSubjectId"
+                  element={<ExamScheduleEdit />}
+                />
 
-                  {/*Lectures*/}
-                  <Route
-                    path="/lecturers/create"
-                    element={<LecturesCreate />}
-                  />
-                  <Route
-                    path="/lecturers/edit/:lecturerId"
-                    element={<LecturersEdit />}
-                  />
+                {/*Lectures*/}
+                <Route path="/lecturers/create" element={<LecturesCreate />} />
+                <Route
+                  path="/lecturers/edit/:lecturerId"
+                  element={<LecturersEdit />}
+                />
 
-                  {/*Subjects*/}
-                  <Route path="/subjects/create" element={<SubjectCreate />} />
-                  <Route
-                    path="/subjects/edit/:subjectId"
-                    element={<SubjectEdit />}
-                  />
-                </Routes>
-              </Suspense>
-            </div>
+                {/*Subjects*/}
+                <Route path="/subjects/create" element={<SubjectCreate />} />
+                <Route
+                  path="/subjects/edit/:subjectId"
+                  element={<SubjectEdit />}
+                />
+              </Routes>
+            </Suspense>
           </div>
         </div>
-      </BrowserRouter>
+      </div>
     </div>
   );
 };
