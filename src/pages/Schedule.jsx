cb9b2c1edad3ae681b01826f3513045fac851f4a
Nguyen-Tan-Phat <tanphat26112000@gmail.com>
2023-04-12 00:51:23 +0700

    change teacher for Header

--- src/pages/Schedule.jsx
+++ src/pages/Schedule.jsx
@@ -188,7 +188,7 @@ const Schedule = () => {
 
   const fetchSchedule = () => {
     fetch(
-      `${BASE_URL_API}/schedule/lecturer/${sessionStorage.getItem("userId")}/schedule`
+      `${BASE_URL_API}/schedule/lecturer/${sessionStorage.getItem("userId")}`
     )
       .then((res) => {
         return res.json();
