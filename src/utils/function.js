19d67aa9cf48c1d26553ed6021f914de00db58c4
Bao Gia <baogpham@kms-technology.com>
2023-04-29 21:11:29 +0700

    Realtime data change

--- src/utils/function.js
+++ src/utils/function.js
@@ -1,33 +1,34 @@
 function sendNotification(message, user) {
-  document.onvisibilitychange = () => {
-    if (document.hidden) {
-      const notification = new Notification(
-        "There is new notification open web to view",
-        {
-          icon: "https://cdn-icons-png.flaticon.com/512/733/733585.png",
-          body: `${user}: ${message}`,
+    document.onvisibilitychange = () => {
+        if (document.hidden) {
+            const notification = new Notification("There is new notification open web to view", {
+                icon: "https://cdn-icons-png.flaticon.com/512/733/733585.png", body: `${user}: ${message}`,
+            });
+            notification.onclick = () => function () {
+                window.open("http://localhost:3000");
+            };
         }
-      );
-      notification.onclick = () =>
-        function () {
-          window.open("http://localhost:3000");
-        };
-    }
-  };
+    };
 }
 
 export default function checkPageStatus(message, user) {
-  if (user === sessionStorage.getItem("fullName")) {
-    if (!("Notification" in window)) {
-      alert("This browser does not support system notifications!");
-    } else if (Notification.permission === "granted") {
-      sendNotification(message, user);
-    } else if (Notification.permission !== "denied") {
-      Notification.requestPermission((permission) => {
-        if (permission === "granted") {
-          sendNotification(message, user);
+    if (user === sessionStorage.getItem("fullName")) {
+        if (!("Notification" in window)) {
+            alert("This browser does not support system notifications!");
+        } else if (Notification.permission === "granted") {
+            sendNotification(message, user);
+        } else if (Notification.permission !== "denied") {
+            Notification.requestPermission((permission) => {
+                if (permission === "granted") {
+                    sendNotification(message, user);
+                }
+            });
         }
-      });
     }
-  }
+}
+
+export const dataChange = (socket) => {
+    socket.emit("dataChange", {
+        message: 'Change',
+    });
 }
