9532e95dd42f77a0dc2e4c7e9c666e6063c6c23b
Nguyen-Tan-Phat <tanphat26112000@gmail.com>
2023-05-04 10:08:22 +0700

    update dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { overviewData } from "../data/dummy";
 import banner from "../assets/banner.png";
 import { useStateContext } from "../contexts/ContextProvider";
+import { BASE_URL_API } from "../utils/constants";
 
 const Dashboard = () => {
   const { currentColor } = useStateContext();
   const fullName = sessionStorage.getItem("fullName");
+  const [totalExam,setTotalExam]=useState("");
+  const fetchSubject = () => {
+    fetch(
+      `${BASE_URL_API}/statistic/StatisticalForLecturerOrLeader/${sessionStorage.getItem(
+        "userId"
+      )}?semesterId=0`
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then((resp) => {
+        setTotalExam(resp);
+        console.log(resp)
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+  
+  useEffect(() => {
+    fetchSubject();
+  }, []);
+
   return (
     <div className="mt-12 flex flex-col items-center">
       <div className="flex flex-wrap lg:flex-nowrap justify-center">
@@ -32,30 +56,122 @@ const Dashboard = () => {
         </div>
       </div>
       <div className="text-lg font-bold">Summary</div>
-
+      {sessionStorage.getItem("roleName")!== "Header"?( 
       <div className="flex m-3 flex-wrap justify-center gap-14 items-center">
-        {overviewData.map((item) => (
-          <div
-            key={item.title}
+        <div
             className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56 p-4 pt-9 rounded-2xl shadow-md"
           >
             <button
               type="button"
-              style={{ color: item.iconColor, backgroundColor: item.iconBg }}
+              style={{ color: "blue", backgroundColor: "yellow" }}
               className="text-2xl opacity-0.9 rounded-full p-4 hover:drop-shadow-xl"
             >
-              {item.icon}
+              {/* {item.icon} */}
             </button>
             <p className="mt-3">
-              <span className="text-lg font-semibold">{item.amount}</span>
-              <span className={`text-sm text-${item.pcColor} ml-2`}>
-                {item.percentage}
+              <span className="text-lg font-semibold">{totalExam.totalExamNeedApprove}</span>
+              <span className={`text-sm text-white ml-2`}>
+                {totalExam.totalExamNeedApprove}
               </span>
             </p>
-            <p className="text-sm text-gray-400 mt-1">{item.title}</p>
+            <p className="text-sm text-gray-400 mt-1">Total Exam Need Approve</p>
           </div>
-        ))}
+          <div
+            className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56 p-4 pt-9 rounded-2xl shadow-md"
+          >
+            <button
+              type="button"
+              style={{ color: "blue", backgroundColor: "yellow" }}
+              className="text-2xl opacity-0.9 rounded-full p-4 hover:drop-shadow-xl"
+            >
+              {/* {item.icon} */}
+            </button>
+            <p className="mt-3">
+              <span className="text-lg font-semibold">{totalExam.totalExamNeedSubmit}</span>
+              <span className={`text-sm text-white ml-2`}>
+                {totalExam.totalExamNeedSubmit}
+              </span>
+            </p>
+            <p className="text-sm text-gray-400 mt-1">Total Exam Need Submit</p>
+          </div>
+          <div
+            className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56 p-4 pt-9 rounded-2xl shadow-md"
+          >
+            <button
+              type="button"
+              style={{ color: "blue", backgroundColor: "yellow" }}
+              className="text-2xl opacity-0.9 rounded-full p-4 hover:drop-shadow-xl"
+            >
+              {/* {item.icon} */}
+            </button>
+            <p className="mt-3">
+              <span className="text-lg font-semibold">{totalExam.totalClassTeaching}</span>
+              <span className={`text-sm text-white ml-2`}>
+                {totalExam.totalClassTeaching}
+              </span>
+            </p>
+            <p className="text-sm text-gray-400 mt-1">Total Class Teaching</p>
+          </div>
       </div>
+      ) : null}
+      {sessionStorage.getItem("roleName")=== "Header"?( 
+      <div className="flex m-3 flex-wrap justify-center gap-14 items-center">
+        <div
+            className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56 p-4 pt-9 rounded-2xl shadow-md"
+          >
+            <button
+              type="button"
+              style={{ color: "blue", backgroundColor: "yellow" }}
+              className="text-2xl opacity-0.9 rounded-full p-4 hover:drop-shadow-xl"
+            >
+              {/* {item.icon} */}
+            </button>
+            <p className="mt-3">
+              <span className="text-lg font-semibold">{totalExam.totalExamNeedSubmittedOfHeader}</span>
+              <span className={`text-sm text-white ml-2`}>
+                {totalExam.totalExamNeedSubmittedOfHeader}
+              </span>
+            </p>
+            <p className="text-sm text-gray-400 mt-1">Total Exam Need Submitted Of Header</p>
+          </div>
+          <div
+            className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56 p-4 pt-9 rounded-2xl shadow-md"
+          >
+            <button
+              type="button"
+              style={{ color: "blue", backgroundColor: "yellow" }}
+              className="text-2xl opacity-0.9 rounded-full p-4 hover:drop-shadow-xl"
+            >
+              {/* {item.icon} */}
+            </button>
+            <p className="mt-3">
+              <span className="text-lg font-semibold">{totalExam.totalExamNotSubmitOfTeacher}</span>
+              <span className={`text-sm text-white ml-2`}>
+                {totalExam.totalExamNotSubmitOfTeacher}
+              </span>
+            </p>
+            <p className="text-sm text-gray-400 mt-1">Total Exam Not Submit Of Teacher</p>
+          </div>
+          <div
+            className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56 p-4 pt-9 rounded-2xl shadow-md"
+          >
+            <button
+              type="button"
+              style={{ color: "blue", backgroundColor: "yellow" }}
+              className="text-2xl opacity-0.9 rounded-full p-4 hover:drop-shadow-xl"
+            >
+              {/* {item.icon} */}
+            </button>
+            <p className="mt-3">
+              <span className="text-lg font-semibold">{totalExam.totalExamNeedSubmittedOfTeacher}</span>
+              <span className={`text-sm text-white ml-2`}>
+                {totalExam.totalExamNeedSubmittedOfTeacher}
+              </span>
+            </p>
+            <p className="text-sm text-gray-400 mt-1">Total Exam Need Submitted Of Teacher</p>
+          </div>
+      </div>
+      ) : null}
     </div>
   );
 };
